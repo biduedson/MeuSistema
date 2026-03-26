bba4282e4c87b4b9ff3a96e936b44ef9f4f4e14d
Edson Gomes <biduzao.bidu21@gmail.com>
2026-03-26 19:24:32 -0300

    feat: adicionar primitivos IEntity e BaseEntity

---
+++ MeuSistema.Domain/Shared/Primitives/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuSistema.Domain.Shared.Primitives;
+
+public abstract class BaseEntity : IEntity<Guid>
+{
+    protected BaseEntity() => Id = Guid.NewGuid();
+
+    protected BaseEntity(Guid id) => Id = id;
+
+    public Guid Id { get; private init; }
+}
+
