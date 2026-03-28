f1220f7783d45efbb1f50d33e560ab22eb0d8610
Edson Gomes <biduzao.bidu21@gmail.com>
2026-03-28 01:40:46 -0300

    feat(domain): add CustomerFactory to create Customer instances

---
+++ MeuSistema.Domain/Factories/CustomerFactory.cs
@@ -0,0 +1,28 @@
+﻿
+
+using MeuSistema.Domain.Entities.CustumerAggregate;
+using MeuSistema.Domain.Shared.Primitives;
+using MeuSistema.Domain.ValueObjects;
+
+namespace MeuSistema.Domain.Factories
+{
+    public static class CustomerFactory
+    {
+        public static DomainResult<Customer> Create(
+            string firstName,
+            string lastName,
+            EGender gender,
+            string email,
+            DateTime dateOfBirth)
+        {
+          var emailResult = Email.Create(email);
+            return !emailResult.IsSuccess
+                 ? DomainResult<Customer>.Failure(emailResult.Error!)
+                 : DomainResult<Customer>.Success(new Customer(firstName, lastName, gender, emailResult.Value!, dateOfBirth));
+                    
+        }
+
+        public static Customer Create(string firstName, string lastName, EGender gender, Email email, DateTime dateOfBirth)
+             => new(firstName, lastName, gender, email, dateOfBirth);
+    }
+}
