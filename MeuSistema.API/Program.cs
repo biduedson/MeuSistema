8495454b0f5fce62af6847e9ae9bcaa201e9a999
Edson Gomes <biduzao.bidu21@gmail.com>
2026-03-29 01:33:22 -0300

    feat: add Scalar API reference at /docs with dark mode enabled

--- MeuSistema.API/Program.cs
+++ MeuSistema.API/Program.cs
@@ -1,5 +1,5 @@
-
 using MeuSistema.Infrastructure;
+using Scalar.AspNetCore;
 
 namespace MeuSistema.API
 {
@@ -9,28 +9,27 @@ namespace MeuSistema.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             builder.Services.AddInfrastructure(builder.Configuration);
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+
+                app.MapScalarApiReference("/docs", options =>
+                {
+                    options.DarkMode = true;
+                    options.Title = "MeuSistema API";
+                });
+
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
