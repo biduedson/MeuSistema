dd6282810e1b8012fd5ec800fc90db834d5c5e3d
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-16 17:09:11 -0300

    fix(solution): correct project paths for test projects
    Updated MeuSistema.slnx to fix incorrect duplicate paths for UnitTests and IntegrationTests projects.
    Removed extra folder nesting in project references so the solution can properly restore, build, and run tests.

---
+++ tests/MeuSistema.UnitTests/Application/Customer/Handlers/CreateCustomerCommandHandlerTests.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Ardalis.Result;
+using Bogus;
+using FluentAssertions;
+using MediatR;
+using MeuSistema.Application.Customer.Commands;
+using MeuSistema.Application.Customer.Handlers;
+using MeuSistema.Domain.Entities.CustumerAggregate;
+using MeuSistema.Infrastructure.Data;
+using MeuSistema.Infrastructure.Data.Repositories;
+using MeuSistema.SharedKernel.Primitives;
+using MeuSistema.UnitTests.Fixtures;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit.Categories;
+
+namespace MeuSistema.UnitTests.Application.Customer.Handlers;
+
+[UnitTest]
+public class CreateCustomerCommandHandlerTests(EfSqliteFixture fixture) : IClassFixture<EfSqliteFixture>
+{
+    private readonly CreateCustomerCommandValidator _validator = new();
+
+    [Fact]
+    public async Task add_ValidCommand_ShouldReturnsCreatedResult()
+    {
+
+        var command = new Faker<CreateCustomerCommand>()
+            .RuleFor(command => command.FirstName, faker => faker.Person.FirstName)
+            .RuleFor(command => command.LastName, faker => faker.Person.LastName)
+            .RuleFor(command => command.Gender, faker => faker.PickRandom<EGender>())
+            .RuleFor(command => command.Email, faker => faker.Person.Email.ToLowerInvariant())
+            .RuleFor(command => command.BirthDate, faker => faker.Person.DateOfBirth)
+            .Generate();
+
+        var unitOfWork = new UnitOfWork(
+            fixture.Context,
+            Substitute.For<IEventStoreRepository>(),
+            Substitute.For<IMediator>(),
+            Substitute.For<ILogger<UnitOfWork>>());
+
+        var handler = new CreateCustomerCommandHandler(
+        _validator,
+        new CustomerRepository (fixture.Context),
+        unitOfWork);
+
+        var act = await handler.Handle(command, CancellationToken.None);
+
+        act.Should().NotBeNull();
+        act.IsCreated().Should().BeTrue();
+        act.Value.Should().NotBeNull();
+        act.Value.Id.Should().NotBe(Guid.Empty);
+    }
+}
